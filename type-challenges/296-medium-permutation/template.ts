54f8f0d636e2e61fbeef7d1ca961b9dfc20230a9
hyy <1014873097@qq.com>
2022-05-18 11:24:35 +0800

    feat(add 296-medium-permutation):我太菜了，不是很理解

---
+++ type-challenges/296-medium-permutation/template.ts
@@ -0,0 +1,6 @@
+// https://github.com/type-challenges/type-challenges/issues/614
+type Permutation<T, U = T> = [T] extends [never]
+  ? []
+  : T extends U
+  ? [T, ...Permutation<Exclude<U, T>>]
+  : never;
