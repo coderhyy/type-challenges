80087e3f339b3df3622c99556d57c1c79a08e1ae
hyy <1014873097@qq.com>
2022-06-05 16:12:17 +0800

    feat: add 0472-hard-tuple-to-enum-object

---
+++ type-challenges/0472-hard-tuple-to-enum-object/template.ts
@@ -0,0 +1,18 @@
+// 参考 https://github.com/type-challenges/type-challenges/issues/10450
+
+// 1.计算当前元素的索引
+type GetIndex<
+  T extends readonly unknown[],
+  K,
+  Rest extends unknown[] = []
+> = T extends readonly [infer F, ...infer R]
+  ? F extends K
+    ? Rest["length"]
+    : GetIndex<R, K, [...Rest, F]>
+  : never;
+
+type Enum<T extends readonly string[], N extends boolean = false> = {
+  readonly [K in T[number] as Capitalize<K>]: N extends true
+    ? GetIndex<T, K>
+    : K;
+};
