6222a59fe13dc613a4db5fbf9fa8db17bcc56ef9
hyy <1014873097@qq.com>
2022-06-05 16:13:53 +0800

    feat: add 0472-hard-tuple-to-enum-object

--- type-challenges/0472-hard-tuple-to-enum-object/template.ts
+++ type-challenges/0472-hard-tuple-to-enum-object/template.ts
@@ -1,4 +1,5 @@
-// 参考 https://github.com/type-challenges/type-challenges/issues/10450
+// 参考 https://github.com/type-challenges/type-challenges/issues/10450 6 Methods and Summaries
+// 参考 https://github.com/type-challenges/type-challenges/issues/10909
 
 // 1.计算当前元素的索引
 type GetIndex<
